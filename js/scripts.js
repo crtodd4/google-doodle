2c62619bb2c7cebe9a645faa93c8cd9150b45c14
Cameron Todd <crtodd4@gmail.com>
2021-03-15 12:45:41 -0700

    Added animations + extra SVG I was saving most of my work on codepen and not commiting the changes to github. I need to do better on that

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-console.log('OK');
+console.log("OK");
+
+gsap.from(".G", {
+  y: -800,
+  duration: 0.5
+});
+
+gsap.from(".g", {
+  y: -800,
+  duration: 1
+});
+
+gsap.from(".l", {
+  y: -800,
+  duration: 1.5
+});
+
+gsap.from(".e", {
+  y: -800,
+  duration: 2
+});
